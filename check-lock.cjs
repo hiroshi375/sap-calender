c174bcc4041eca7dc0d8b2eb8ed8dd2f3c8d0351
hiroshi375 <68233466+hiroshi375@users.noreply.github.com>
2026-07-11 01:59:47 +0900

    Delete check-lock.cjs

--- check-lock.cjs
+++
@@ -1,5 +0,0 @@
-const l = require("./package-lock.json");
-
-console.log("lockfileVersion =", l.lockfileVersion);
-console.log("root =", l.packages[""]);
-console.log("has dependencies object =", !!l.dependencies);
